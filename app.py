2dcdd005897e432030f124acff1a97631b87fcf4
Muhammad Abdur Rafey Ahmed <80873403+mabdurrafey1@users.noreply.github.com>
2023-03-31 20:29:04 +0500

    update

--- app.py
+++ app.py
@@ -8,19 +8,31 @@ def index():
 
 @application.route('/check_overlap', methods=['POST'])
 def check_overlap():
-    data = request.get_json()
-    range1_start = data['range1']['start']
-    range1_end = data['range1']['end']
-    range2_start = data['range2']['start']
-    range2_end = data['range2']['end']
+    try:
+        data = request.get_json()
+        
+        if not data:
+            raise ValueError("No data provided")
 
-    status= range1_start < range2_end and range1_end > range2_start
+        range1_start = data['range1']['start']
+        range1_end = data['range1']['end']
+        range2_start = data['range2']['start']
+        range2_end = data['range2']['end']
+
+        status= range1_start < range2_end and range1_end > range2_start
+        
+        response = {
+            'overlap': status
+        }
+        
+        return jsonify(response)
     
-    response = {
-        'overlap': status
-    }
-    
-    return jsonify(response)
+    except KeyError as e:
+        return jsonify({'error': f'Missing key: {e.args[0]}' }), 400
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occurred'}), 500
 
 if __name__ == '__main__':
     application.run(debug=True)
